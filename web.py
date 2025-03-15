a521ce1719be052e4a36299c9f60e4c2ab53534b
PUGE <mail@puge.org>
2025-03-15 12:36:51 +0800

    数据错误也能显示图标

--- web.py
+++ web.py
@@ -57,7 +57,10 @@ def getTable():
     df = pd.read_excel('./file.xlsx')
 
     # 将 DataFrame 转换为 HTML
+    # 设置索引从 1 开始
+    df.index = range(1, len(df) + 1)
     html_data = df.to_html()
+    
     html_data = html_data.replace('NaN', '')
     return html_data
 
@@ -121,71 +124,89 @@ def getData():
         return [date1 < date2]
     for row in sheet.iter_rows(min_row=2, values_only=True):
         if (row[0] != None):
-            # 项目数量
-            outputData["节点统计到期图纸"][getDateStr(row[15])] += 1
-            outputData["节点统计到期预算"][getDateStr(row[18])] += 1
-            outputData["节点统计到期采购"][getDateStr(row[21])] += 1
-            outputData["节点统计到期制造"][getDateStr(row[24])] += 1
-            outputData["节点统计到期检验"][getDateStr(row[27])] += 1
-            outputData["节点统计到期发运"][getDateStr(row[30])] += 1
-            # 延期数量
-            if (checkDate(row[15], row[16])):
-                outputData["节点统计超期图纸"][getDateStr(row[15])] += 1
-            if (checkDate(row[18], row[19])):
-                outputData["节点统计超期预算"][getDateStr(row[18])] += 1
-            if (checkDate(row[21], row[22])):
-                outputData["节点统计超期采购"][getDateStr(row[21])] += 1
-            if (checkDate(row[24], row[25])):
-                outputData["节点统计超期制造"][getDateStr(row[24])] += 1
-            if (checkDate(row[27], row[28])):
-                outputData["节点统计超期检验"][getDateStr(row[27])] += 1
-            if (checkDate(row[30], row[31])):
-                outputData["节点统计超期发运"][getDateStr(row[30])] += 1
-            # 收款详情
-            if (row[45]):
-                outputData["总已收账款"] += int(row[45])
-            if (row[39]):
-                outputData["总应收账款"] += int(row[39])
-            if (row[44]):
-                if (row[44] == '预付款'):
-                    outputData["待收款性质"][0] += 1
-                    outputData["待收款金额"][0] += int(row[39])
-                if (row[44] == '进度款'):
-                    outputData["待收款性质"][1] += 1
-                    outputData["待收款金额"][1] += int(row[39])
-                if (row[44] == '提货款'):
-                    outputData["待收款性质"][2] += 1
-                    outputData["待收款金额"][2] += int(row[39])
-                if (row[44] == '到货款'):
-                    outputData["待收款性质"][3] += 1
-                    outputData["待收款金额"][3] += int(row[39])
-                if (row[44] == '调试款'):
-                    outputData["待收款性质"][4] += 1
-                    outputData["待收款金额"][4] += int(row[39])
-                if (row[44] == '验收款'):
-                    outputData["待收款性质"][5] += 1
-                    outputData["待收款金额"][5] += int(row[39])
-                if (row[44] == '质保金'):
-                    outputData["待收款性质"][6] += 1
-                    outputData["待收款金额"][6] += int(row[39])
-            if (row[14]):
-                outputData['项目状态'][0] += 1
-                outputData["完成情况"][row[14]] += 1
-            else:
-                outputData['项目状态'][1] += 1
-                outputData["完成情况"]["进行中"] += 1
-            # 是否超期
-            if (row[38]):
-                outputData['项目状态'][2] += 1
-            else:
-                outputData['项目状态'][3] += 1
-            # 罚款关注
-            if (row[39]):
-                outputData['项目状态'][4] += 1
-            if (row[33]):
-                outputData['项目状态'][5] += 1
+            
+            try:
+                # 项目数量
+                outputData["节点统计到期图纸"][getDateStr(row[15])] += 1
+                outputData["节点统计到期预算"][getDateStr(row[18])] += 1
+                outputData["节点统计到期采购"][getDateStr(row[21])] += 1
+                outputData["节点统计到期制造"][getDateStr(row[24])] += 1
+                outputData["节点统计到期检验"][getDateStr(row[27])] += 1
+                outputData["节点统计到期发运"][getDateStr(row[30])] += 1
+                # 延期数量
+                if (checkDate(row[15], row[16])):
+                    outputData["节点统计超期图纸"][getDateStr(row[15])] += 1
+                if (checkDate(row[18], row[19])):
+                    outputData["节点统计超期预算"][getDateStr(row[18])] += 1
+                if (checkDate(row[21], row[22])):
+                    outputData["节点统计超期采购"][getDateStr(row[21])] += 1
+                if (checkDate(row[24], row[25])):
+                    outputData["节点统计超期制造"][getDateStr(row[24])] += 1
+                if (checkDate(row[27], row[28])):
+                    outputData["节点统计超期检验"][getDateStr(row[27])] += 1
+                if (checkDate(row[30], row[31])):
+                    outputData["节点统计超期发运"][getDateStr(row[30])] += 1
+                # 收款详情
+                if (row[45]):
+                    outputData["总已收账款"] += int(row[45])
+                if (row[39]):
+                    outputData["总应收账款"] += int(row[39])
+                if (row[44]):
+                    if (row[44] == '预付款'):
+                        outputData["待收款性质"][0] += 1
+                        outputData["待收款金额"][0] += int(row[39])
+                    if (row[44] == '进度款'):
+                        outputData["待收款性质"][1] += 1
+                        outputData["待收款金额"][1] += int(row[39])
+                    if (row[44] == '提货款'):
+                        outputData["待收款性质"][2] += 1
+                        outputData["待收款金额"][2] += int(row[39])
+                    if (row[44] == '到货款'):
+                        outputData["待收款性质"][3] += 1
+                        outputData["待收款金额"][3] += int(row[39])
+                    if (row[44] == '调试款'):
+                        outputData["待收款性质"][4] += 1
+                        outputData["待收款金额"][4] += int(row[39])
+                    if (row[44] == '验收款'):
+                        outputData["待收款性质"][5] += 1
+                        outputData["待收款金额"][5] += int(row[39])
+                    if (row[44] == '质保金'):
+                        outputData["待收款性质"][6] += 1
+                        outputData["待收款金额"][6] += int(row[39])
+                if (row[14]):
+                    outputData['项目状态'][0] += 1
+                    outputData["完成情况"][row[14]] += 1
+                else:
+                    outputData['项目状态'][1] += 1
+                    outputData["完成情况"]["进行中"] += 1
+                # 是否超期
+                if (row[38]):
+                    outputData['项目状态'][2] += 1
+                else:
+                    outputData['项目状态'][3] += 1
+                # 罚款关注
+                if (row[39]):
+                    outputData['项目状态'][4] += 1
+                if (row[33]):
+                    outputData['项目状态'][5] += 1
+            except:
+                print('数据错误:')
+                print(row)
     print(outputData)
     return json.dumps(outputData)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part", 400
+
+    file = request.files['file']
+    if file.filename != 'file.xlsx':
+        return "No selected file", 400
+
+    # 保存文件
+    file.save(f"./{file.filename}")
+    return "File uploaded successfully!", 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host="0.0.0.0")
